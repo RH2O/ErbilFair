7dc0e808a78d9ca30bdd8f56f8ed8418aa46bdb3
RH2O <abdrhf20@gmail.com>
2021-08-13 16:46:05 +0300

    firs version
    erbil int. fair

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+$dsn ='mysql:host=localhost;dbname=erbilfair';
+$user = 'root';
+$pass ='';
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+);
+
+
+try {
+    $db = new PDO($dsn, $user, $pass, $options);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
